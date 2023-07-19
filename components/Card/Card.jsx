5f936f55b24b7a356f345c493262b0e9069b5b4e
leolegrand <88097360+leolegrand@users.noreply.github.com>
2023-07-19 13:55:47 +0200

    flipping effect need adjustements

--- components/Card/Card.jsx
+++ components/Card/Card.jsx
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from "./component.module.css"
 
 const Card = ({projet, index}) => {
+
+    const [isOpen, setIsOpen] = useState(false)
+
+    const handleCard = () => {
+        setIsOpen(!isOpen)
+    }
+
     return (
-        <article className={styles.card + " slide"} key={index}>
-            <div className={styles.header}>
-                <p className={styles.title}>{projet.title}</p>
-                <div className={styles.separationLine}></div>
+        <article className={isOpen? styles.card + " " + styles.cardOpen + " slide" : styles.card + " slide"} key={index} onClick={handleCard}>
+            <div className={styles.front}>
+                <div className={styles.header}>
+                    <p className={styles.title}>{projet.title}</p>
+                    <div className={styles.separationLine}></div>
+                </div>
+                <img src={projet.logoUrl} alt={projet.title +  " logo"}/>
+                <ul className={styles.tagsList}>
+                    {projet.categories.map((categorie, index) => (
+                    <li className={styles.tag} key={index}>{categorie.title}</li>))}
+                </ul>
+                </div>
+            <div className={styles.back}>
+                <p>BACK FACE</p>
             </div>
-            <img src={projet.logoUrl} alt={projet.title +  " logo"}/>
-            <ul className={styles.tagsList}>
-                {projet.categories.map((categorie, index) => (
-                <li className={styles.tag} key={index}>{categorie.title}</li>))}
-             </ul>
-            
         </article>
     );
 };
